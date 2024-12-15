41ab10fc77c7dec1e03ac486afe507436a5fdb68
Adrien Meyrand <adrien.meyrand@gmail.com>
2024-12-15 19:06:37 +0100

    second commit

---
+++ scripts.js
@@ -0,0 +1,20 @@
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+    console.log("btn cliqué");
+    document.body.scrollTop = 0;
+}
+
+
+function scrollReveal() {
+
+    ["s-right", "s-left", "s-top", "s-bottom"].forEach((className) => {
+        ScrollReveal().reveal(`.${className}`, {
+            delay: 100,
+            duration: 1000,
+            distance: "5%",
+            origin: className.replace("s-", ""),
+            reset: true
+        });
+    });
+
+}
